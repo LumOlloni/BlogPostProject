bdfce4db2042bf4e43fbbef490d08a37cde661ba
Lum <lum-olloni@hotmail.com>
2019-10-15 12:02:35 +0200

    db seed

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+        foreach (range(1,5) as $index) {
+            DB::table('users')->insert([
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'email_verified_at' => now(),
+                'email_verified_at' => now(),
+                'role_id' => $faker->numberBetween(1, 2),
+                'password' => bcrypt('123456789'),
+                'remember_token' => Str::random(10),
+            ]);
+        }
+    }
+}
